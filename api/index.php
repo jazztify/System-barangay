b13e0e86aa076e6908fc7ed5a7358b3cfba8831e
jazztify <jazztify1738l@gmail.com>
2026-05-15 09:58:57 +0800

    Enable error reporting for Vercel debugging

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
+// Enable error reporting for debugging on Vercel
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Forward request to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
